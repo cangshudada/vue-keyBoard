a6cde0bd45e92fd17026425cce994d3377fe2285
yangQi <15342240569@163.com>
2021-04-26 09:56:58 +0800

    feat:release v1.0.2

---
+++ lib/index.d.ts
@@ -0,0 +1,60 @@
+import Vue from "vue";
+
+/** KeyBoard component common definition */
+declare class KeyBoardComponent extends Vue {
+  /** Install component into Vue */
+  static install(vue: typeof Vue): void;
+}
+
+/** KeyBoard Component */
+export declare class KeyBoard extends KeyBoardComponent {
+  /** Binding value */
+  value: string | number;
+
+  /** theme color - support rgb or hex - default("#eaa050") */
+  color: string;
+
+  /** mode list - support symbol or handwrite keyboard - default(["handwrite", "symbol"]) */
+  modeList: ("handwrite" | "symbol")[];
+
+  /** is blur status hide - default(true) */
+  blurHide: boolean;
+
+  /** Whether to show the drag handle - default(true)  */
+  showHandleBar: boolean;
+
+  /** handwrite interface */
+  handApi: string;
+
+  /** transition className */
+  animateClass: string;
+
+  /** darg Handle text */
+  dargHandleText: string;
+
+  /** modal exist status */
+  modal: boolean;
+
+  /** modal can hide when click modal */
+  closeOnClickModal: boolean;
+
+  /**
+   * key change
+   */
+  keyChange(value: string): void;
+
+  /**
+   * value change
+   */
+  change(value: string): void;
+
+  /**
+   * keyboard close hook
+   */
+  closed(): void;
+
+  /**
+   * keyboard close hook
+   */
+  modalClick(): void;
+}
